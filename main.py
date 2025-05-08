4bc725af5a0683f80bedfc4e9a6a60e596f08ecb
vyacheslavfitsko <peygur.fitsko@gmail.com>
2025-05-08 09:48:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot
+from datetime import datetime
+import random
+import os
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+users = {}
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_id = message.chat.id
+    today = datetime.now().strftime("%Y-%m-%d")
+
+    if users.get(user_id) == today:
+        bot.send_message(user_id, "😬 Вы уже получали промокод сегодня. Попробуйте снова завтра!")
+    else:
+        promo = random.choice(["LUCKY5", "LUCKY10", "LUCKY15", "LUCKY20"])
+        users[user_id] = today
+        bot.send_message(user_id, f"🎉 Поздравляем! Ваш промокод: <b>{promo}</b>", parse_mode="HTML")
+
+print("✅ Бот запущен...")
+bot.polling()
