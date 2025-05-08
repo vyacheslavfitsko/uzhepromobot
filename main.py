8758b04adb6248fbd9c69f7444c0c2a80a476f60
vyacheslavfitsko <peygur.fitsko@gmail.com>
2025-05-08 09:59:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
 import telebot
-from datetime import datetime
-import random
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-bot = telebot.TeleBot(8072658630:AAFafvZi8EF0Qkjjzacv0lfkB5W9A1zHwUg)
-
-users = {}
+bot = telebot.TeleBot(os.getenv("TELEGRAM_TOKEN"))
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    user_id = message.chat.id
-    today = datetime.now().strftime("%Y-%m-%d")
+    bot.send_message(message.chat.id, "Привет! Я работаю!")
 
-    if users.get(user_id) == today:
-        bot.send_message(user_id, "😬 Вы уже получали промокод сегодня. Попробуйте снова завтра!")
-    else:
-        promo = random.choice(["LUCKY5", "LUCKY10", "LUCKY15", "LUCKY20"])
-        users[user_id] = today
-        bot.send_message(user_id, f"🎉 Поздравляем! Ваш промокод: <b>{promo}</b>", parse_mode="HTML")
-
-print("✅ Бот запущен...")
 bot.polling()
